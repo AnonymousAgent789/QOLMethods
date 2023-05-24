f5a015827315bc3fc2190afa496039a63be0f7e7
Anonymous Agent <121668381+AnonymousAgent789@users.noreply.github.com>
2023-05-24 19:09:00 +0100

    Update IntegerExtensions.java

--- QOLMethods/Extensions/IntegerExtensions.java
+++ QOLMethods/Extensions/IntegerExtensions.java
@@ -2,7 +2,7 @@ package QOLMethods.Extensions;
 
 public abstract class IntegerExtensions {
 	
-	public static int factorial(int num) {
+	public static long factorial(int num) {
 		
 		if (num == 0) {
 			return 1;
